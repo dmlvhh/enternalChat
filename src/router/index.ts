823426ccd157398de5a25b890d935605fc6b73bf
lv <1967798998@qq.com>
2024-10-06 18:30:55 +0800

    feature:设置-用户信息修改和密码修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -65,7 +65,25 @@ const router = createRouter({
         {
           path: '/setting',
           name: '设置',
-          component: () => import('@/views/setting/Setting.vue')
+          redirect: '/setting/userInfo',
+          component: () => import('@/views/setting/Setting.vue'),
+          children: [
+            {
+              path: '/setting/userInfo',
+              name: '个人信息',
+              component: () => import('@/views/setting/UserInfo.vue')
+            },
+            {
+              path: '/setting/fileManage',
+              name: '文件管理',
+              component: () => import('@/views/setting/FileManage.vue')
+            },
+            {
+              path: '/setting/about',
+              name: '关于',
+              component: () => import('@/views/setting/About.vue')
+            }
+          ]
         }
       ]
     }
